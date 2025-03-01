f1100dce8379f7596679b45bdd791f6e1233d7cd
Roman Vazhynskyi <61057791+rovazh@users.noreply.github.com>
2025-03-01 15:45:04 +0200

    Updated year (#6)

--- src/Buffer.php
+++ src/Buffer.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the PhpSocks package.
  *
- * (c) 2024 Roman Vazhynskyi
+ * (c) 2025 Roman Vazhynskyi
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
